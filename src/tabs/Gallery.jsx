1a51fbb7154c171fa386a08cfd838527f006bcbd
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-01-20 17:05:23 +0200

    gallery: fetch, render open modal

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -8,6 +8,8 @@ export const Gallery = () => {
   const [page, setPage] = useState(1);
   const [images, setImages] = useState([]);
   const [isLastPage, setIsLastPage] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const [dataForModal, setDataForModal] = useState(null);
 
   const onSubmit = (query) => {
     setQuery(query);
@@ -26,17 +28,43 @@ export const Gallery = () => {
     fn();
   }, [query, page]);
 
-  const elements = images.map(
-    ({ id, avg_color, alt, src: { medium, large } }) => (
-      <GridItem key={id} id={id}>
-        <CardItem color={avg_color}>
-          <img src={medium} alt={alt} />
-        </CardItem>
-      </GridItem>
-    )
-  );
+  const onToggleModal = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
+  const handleImageClick = ({ large, alt }) => {
+    onToggleModal();
+    setDataForModal({ large, alt });
+  };
+
+  const handleBackdropClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onToggleModal();
+      setDataForModal(null);
+    }
+  };
+
+  const elements = images.map(({ id, avg_color, alt, src: { medium, large } }) => (
+    <GridItem key={id} id={id}>
+      <CardItem color={avg_color}>
+        <img onClick={() => handleImageClick({ large, alt })} src={medium} alt={alt} />
+      </CardItem>
+    </GridItem>
+  ));
   const onClick = () => {
     setPage((prev) => prev + 1);
+  };
+
+  const styles = {
+    position: "fixed",
+    width: "100vw",
+    height: "100vh",
+    top: "0",
+    left: "0",
+    backgroundColor: "rgba(0,0,0,0.6)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   };
 
   return (
@@ -45,7 +73,13 @@ export const Gallery = () => {
       {images.length <= 0 && (
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       )}
-      <Grid>{elements}</Grid>
+      <Grid>{elements} </Grid>
+      {isOpen && (
+        <div onClick={handleBackdropClick} style={styles}>
+          {" "}
+          <img src={dataForModal.large} alt={dataForModal.alt} width={400} height={360} />
+        </div>
+      )}
       {!isLastPage && <Button onClick={onClick}>Load more...</Button>}
     </>
   );
