d37b0ce0b906be0e344118e020dc57dc75f26b5b
NataliaPot <137684728+NataliaPot@users.noreply.github.com>
2024-02-17 15:43:15 +0100

    add-delete-action

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -67,12 +67,7 @@ export const App = () => {
             {todos.length > 0 &&
               todos.map((todo, index) => (
                 <GridItem key={todo.id}>
-                  {/* <Todo
-                    id={todo.id}
-                    text={todo.text}
-                    counter={index + 1}
-                    onClick={this.deleteTodo}
-                  /> */}
+                  <Todo id={todo.id} text={todo.text} counter={index + 1} />
                 </GridItem>
               ))}
           </Grid>
--- src/redux/todo-slice.js
+++ src/redux/todo-slice.js
@@ -7,11 +7,14 @@ const todoSlice = createSlice({
   },
   reducers: {
     addToDO(state, action) {
-    state.todos = [...state.todos, action.payload]
+      state.todos = [...state.todos, action.payload];
+    },
+    deleteTodo(state, action) {
+      state.todos = state.todos.filter(todo => todo.id !== action.payload);
     },
   },
 });
 
-export const { addToDO} = todoSlice.actions;
+export const { addToDO, deleteTodo } = todoSlice.actions;
 
 export const todoReducer = todoSlice.reducer;
